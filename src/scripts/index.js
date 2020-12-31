702bf1a474458002b3f2d115b2dc86e6eea7d289
Seyi <oluseyi.ogunbanjo@gmail.com>
2020-12-31 04:09:41 -0500

    Completed page structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,7 +10,7 @@ function component() {
     return element;
   }
   
-  document.body.appendChild(component());
+  // document.body.appendChild(component());
 
   if (module.hot) {
     module.hot.accept()
