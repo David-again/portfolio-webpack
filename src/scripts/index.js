7f49be4e09b4a6dcd91fc8c18660d0e3ba64bea3
David-again <d.ogunbanjo@yahoo.com>
2021-02-22 23:47:58 -0500

    initial version (functional)... need to remove extraneous files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,36 +2,10 @@ import _ from 'lodash';
 import '../css/styles.css';
 import 'lazysizes';
 import MobileMenu from './modules/Mobile-Menu';
-// import Modal from './modules/Modal';
 
-// const myVillains = require('supervillains');
-// myVillains.all;
-// console.log(myVillains.random());
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactComponent from './modules/ReactComponent';
-
-ReactDOM.render(<ReactComponent />, document.querySelector("#main-doc-react"));
-
-// new Modal();
 let mobileMenu = new MobileMenu();
 let modal;
 
-document.querySelectorAll(".under-construction").forEach(el => {
-  el.addEventListener("click", e => {
-    e.preventDefault();
-    if (typeof modal == "undefined") {
-      import('./modules/Modal').then(x => {
-        modal = new x.default();
-        setTimeout(() => modal.openTheModal(), 20);
-      }).catch(() => console.log("There was a problem."))
-    } else {
-      modal.openTheModal();
-    }
-  })
-})
-
-  if (module.hot) {
-    module.hot.accept()
-  }
+if (module.hot) {
+   module.hot.accept()
+}
