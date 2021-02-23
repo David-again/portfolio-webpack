3200b401f4a4090e38ae1d074262e9ca502ad369
David-again <d.ogunbanjo@yahoo.com>
2021-02-22 23:54:16 -0500

    more extraneous files removed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,5 @@
 import _ from 'lodash';
 import '../css/styles.css';
-import 'lazysizes';
-import MobileMenu from './modules/Mobile-Menu';
-
-let mobileMenu = new MobileMenu();
-let modal;
 
 if (module.hot) {
    module.hot.accept()
