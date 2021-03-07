f402b5fa52adc59505f44fc33af5f5ebb8987d8f
David-again <d.ogunbanjo@yahoo.com>
2021-03-07 04:28:25 -0500

    UI: Progress bar added to redirect page + docs folder updated

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,17 @@
 import _ from 'lodash';
 import '../css/styles.css';
 
+// Countdown (progress bar) for redirection
+var timeleft = 8;
+var redirectTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(redirectTimer);
+  }
+  document.getElementById("progressBar").value = 10 - timeleft;
+  timeleft -= 0.25;
+}, 250);
+
+
 if (module.hot) {
    module.hot.accept()
 }
